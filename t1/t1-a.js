25a2a17c55ff4b45d326dd10ecb86c26f42145d7
Syedmeeran14 <syedmeeranoff@gmail.com>
2023-11-30 14:51:17 +0530

    first commit

---
+++ t1/t1-a.js
@@ -0,0 +1,15 @@
+class movie
+{
+    constructor (title,studio,rating)
+    {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+}
+const movie1 = new movie("kanguva","studio green","PG15");
+const movie2 = new movie("vikram","rkfc","PG13");
+
+console.log(movie1);
+console.log(movie2);
